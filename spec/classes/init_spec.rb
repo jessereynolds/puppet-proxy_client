9ef2265372412cf823811414aff617ea699c5719
Jesse Reynolds <jesse@va.com.au>
2019-08-02 09:12:43 +1200

    use facts for spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 describe 'proxy_client' do
-  context 'with default values for all parameters' do
-    it { is_expected.to contain_class('proxy_client') }
+  on_supported_os.each do |os, os_facts|
+
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'with default values for all parameters' do
+        it { is_expected.to contain_class('proxy_client') }
+      end
+
+    end
+
   end
 end
+
